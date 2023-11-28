1eb2bddbda470d73b3f859c39fbb752fb7bec2ba
ColinSebas <111238679+ColinSebas@users.noreply.github.com>
2023-11-27 20:52:43 -0600

    Correcciones finales

--- main.cpp
+++ main.cpp
@@ -20,6 +20,17 @@
 
 using namespace std;
 
+void menu() {
+
+    cout << "\n1. Agregar imagenes de archivo de lectura\n";
+    cout << "2. Ordenar imagenes por fecha\n";
+    cout << "3. Buscar imagen con base en ID\n";
+    cout << "4. Imprimir lista de imagenes y generar archivo de salida\n";
+    cout << "5. Salir\n";
+    cout << "NO ELEGIR OPCION 4 HASTA ORDENAR LAS IMAGENES\n";
+
+}
+
 int main(int argc, char* argv[]) {
     
     ifstream inputFile;
@@ -38,6 +49,8 @@ int main(int argc, char* argv[]) {
 
     outputFile.open(argv[2]);
 
+    
+
     // Vectores auxiliar para recorrido
     vector<Image> coleccionGral; 
 
@@ -54,43 +67,91 @@ int main(int argc, char* argv[]) {
     float size = 0.0; 
     string date; string ext; string name;
 
+    // Menu nav var
+    int menuOpt = 0;
 
-    inputFile >> IM;
 
-    for (int i = 0; i <= IM; i++) {
 
-        inputFile >> event;
+    while(menuOpt < 5 && menuOpt > -1) {
 
-        switch(event) {
+        menu();
+        cin >> menuOpt;
+
+        switch(menuOpt) {
 
             case 1:
+            {
+                inputFile >> IM;
 
-                inputFile >> size >> date >> ext >> name;
-                coleccionGral.push_back(Image(idImages, size, date, ext, name));
-                idImages++;
+                for (int i = 0; i <= IM; i++) {
+
+                    inputFile >> event;
+
+                    switch(event) {
+
+                        case 1:
+
+                            inputFile >> size >> date >> ext >> name;
+                            coleccionGral.push_back(Image(idImages, size, date, ext, name));
+                            idImages++;
+                            break;
+
+                        default:
+                            break;
+                        }
+
+                }
+
+                outputFile << "BEFORE SORTING\n";
+                for (auto& image : coleccionGral) {
+                    col.insert(image);
+                    outputFile << image.toString();
+                }
+            }    
+            break;
+
+            case 2:
+            {
+                //cout << "BEFORE SORTING\n";
+                //cout << col.toString() << "\n";
+                col.sort();
+                //cout << "\nAFTER SORTING" << "\n";
+                outputFile << "\nAFTER SORTING" << "\n";
+                //cout << col.toString();
+                outputFile << col.toString();
+            }    
                 break;
 
-            default:
-                break;
+            case 3: 
+            {
+                int id_im = 0;
+                cout << "Que ID de imagen quieres buscar? (de 0 a 19)\n";
+                cin >> id_im;
+
+                if (id_im < 20 && id_im > -1) {
+                    col.search(coleccionGral[id_im]);
+                    break;
+                } else {
+                    cout << "ID no valido";
+                    break;
+                }
             }
+            break;
 
-    }
+            case 4:
+                cout << col.toString() << "\n";
+                break;
 
-    for (auto& image : coleccionGral) {
-        col.insert(image);
-        outputFile << image.toString();
-    }
+            case 5:
+                break;  
+
+
+        }
     
-    cout << col.toString();
-    col.sort();
-    cout << "\nAFTER SORTING" << "\n";
-    outputFile << "\nAFTER SORTING" << "\n";
-    cout << col.toString();
-    outputFile << col.toString();
+    }
 
     inputFile.close();
     outputFile.close();
     return 0;
-    
 
 }
